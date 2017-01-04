e68cc68e8e5195f94d82ef8d2016ab82f398db2c
ivanovDi <dima77703011983@gmail.com>
2017-01-04 15:53:38 +0200

    start project

---
+++ app/Employee.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name'
+    ];
+
+    public function positions()
+    {
+        return $this->belongsToMany('App/Position');
+    }
+}
